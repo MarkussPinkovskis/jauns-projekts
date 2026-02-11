96f536930886f232ecf0ae3ecbe7fdc7c946e498
MarkussPinkovskis <markusspinkovskis4@gmail.com>
2026-02-11 09:25:31 +0200

    kautkas

--- static/app.py
+++ static/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template_string, request, redirect, sqlite3
+from flask import Flask, render_template_string, request, redirect
 from datetime import datetime
 import sqlite3
 con = sqlite3.connect("colorgenlogin.db")
+app = Flask(__name__)
+
